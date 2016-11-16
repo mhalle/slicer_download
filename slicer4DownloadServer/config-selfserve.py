cd7999d92859433c9ab8311dc6ab5538c4875802
Michael Halle <m -@- halle.us>
2016-11-16 11:31:12 -0500

    reformatted and mved config files

--- slicer4DownloadServer/config-selfserve.py
+++
@@ -1,3 +0,0 @@
-DEBUG=True
-MIDAS_DB_FILENAME="../data/fallback/midas.sqlite"
-SERVER_NAME='localhost:13131'
--- slicer4DownloadServer/config.py
+++ slicer4DownloadServer/config.py
@@ -1,3 +1,2 @@
-DEBUG=False
-MIDAS_DB_FILENAME="../data/fallback/midas.sqlite"
-
+DEBUG = False
+MIDAS_DB_FILENAME = "../data/fallback/midas.sqlite"
