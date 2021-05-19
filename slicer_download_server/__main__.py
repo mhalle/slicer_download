bed2a7200f900459f0f16eb081dc45c781bfa0d5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-05-19 02:42:44 -0400

    slicer_download_server: Remove unused __main__.py

--- slicer_download_server/__main__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-path = os.path.dirname(sys.modules[__name__].__file__)
-path = os.path.join(path, '..')
-sys.path.insert(0, path)
-from slicerDownloadServer import app
-
-app.run()
