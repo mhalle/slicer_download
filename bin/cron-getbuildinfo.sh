48aa893b7fcf86471355cd724200850d599de1a2
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-06-03 09:56:52 -0400

    bin/cron-getbuildinfo: Refactor script and remove unneeded query
    * Update script to be use ROOT_DIR, VIRTUALENV_DIR and PYTHON_EXECUTABLE variables
    * Remove obsolete query removing revision 5266433

--- bin/cron-getbuildinfo.sh
+++ bin/cron-getbuildinfo.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-ROOT="${HOME}/apps/slicer_download"
-DBFILE="${ROOT}/var/slicer-midas-records.sqlite"
-PYTHON="${ROOT}/env/bin/python"
+set -e
 
-"${PYTHON}" "${ROOT}/etc/slicer_getbuildinfo" ${DBFILE} \
-    && sqlite3 ${DBFILE} 'delete from _ where revision = 5266433;'
+script_dir=$(cd $(dirname $0) || exit 1; pwd)
 
+ROOT_DIR=$(realpath "${script_dir}/..")
+VIRTUALENV_DIR=$(realpath -m "${ROOT_DIR}/env")
+PYTHON_EXECUTABLE=${VIRTUALENV_DIR}/bin/python
+
+DBFILE="${ROOT_DIR}/var/slicer-midas-records.sqlite"
+
+"${PYTHON_EXECUTABLE}" "${ROOT_DIR}/etc/slicer_getbuildinfo" ${DBFILE}
