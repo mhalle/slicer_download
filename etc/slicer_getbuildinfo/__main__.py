e613de333dfaeef43506e629548c7dcd0aa62b39
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-06-03 11:30:20 -0400

    slicer_getbuildinfo: Add support for Girder server

--- etc/slicer_getbuildinfo/__main__.py
+++ etc/slicer_getbuildinfo/__main__.py
@@ -3,6 +3,7 @@ import os
 import urllib.error
 import urllib.parse
 import urllib.request
+import requests
 import sqlite3
 import sys
 import textwrap
@@ -12,6 +13,7 @@ from enum import Enum
 
 class ServerAPI(Enum):
     Midas_v1 = 1
+    Girder_v1 = 2
 
 
 def getServerAPI():
@@ -21,6 +23,7 @@ def getServerAPI():
 def getServerAPIUrl():
     return {
         ServerAPI.Midas_v1: "http://slicer.kitware.com/midas3/api/json",
+        ServerAPI.Girder_v1: "https://slicer-packages.kitware.com/api/v1",
     }[getServerAPI()]
 
 
@@ -36,9 +39,15 @@ def getMidasRecordsFromURL():
     return json.loads(info)['data']
 
 
+def getGirderRecordsFromURL():
+    result = requests.get("{0}/app/5f4474d0e1d8c75dfc705482/package?limit=0".format(getServerAPIUrl()))
+    return result.json()
+
+
 def getRecordsFromURL():
     return {
         ServerAPI.Midas_v1: getMidasRecordsFromURL,
+        ServerAPI.Girder_v1: getGirderRecordsFromURL,
     }[getServerAPI()]()
 
 
@@ -53,9 +62,18 @@ def midasRecordToDb(r):
         return None
 
 
+def girderRecordToDb(r):
+    return [r['_id'],
+            int(r['meta']['revision']),
+            r['created'],
+            r['created'],
+            json.dumps(r)]
+
+
 def recordToDb(r):
     return {
         ServerAPI.Midas_v1: midasRecordToDb,
+        ServerAPI.Girder_v1: girderRecordToDb,
     }[getServerAPI()](r)
 
 
@@ -64,13 +82,16 @@ def main(dbfile):
     print("ServerAPI is {0}: {1}".format(getServerAPI().name, getServerAPIUrl()))
 
     records = getRecordsFromURL()
+
+    primary_key_type = "INTEGER" if getServerAPI() == ServerAPI.Midas_v1 else "TEXT"
+
     with sqlite3.connect(dbfile) as db:
         db.execute('''create table if not exists
-        _(item_id INTEGER primary key,
+        _(item_id {primary_key_type} primary key,
                     revision INTEGER,
                     checkout_date TEXT,
                     build_date TEXT,
-                    record TEXT)''')
+                    record TEXT)'''.format(primary_key_type=primary_key_type))
 
         cursor = db.cursor()
         cursor.executemany('''insert or ignore into _
