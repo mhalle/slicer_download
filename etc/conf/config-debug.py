f3be9be7ceb987acb9d45c85cd274d34b37b969a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-06-04 09:45:48 -0400

    slicer_download_server: Support setting SLICER_DOWNLOAD_DB_FILE env. var

--- etc/conf/config-debug.py
+++ etc/conf/config-debug.py
@@ -1,2 +1,4 @@
+import os
+
 DEBUG = True
-DB_FILE = "../etc/fallback/slicer-midas-records.sqlite"
+DB_FILE = os.environ.get("SLICER_DOWNLOAD_DB_FILE", "../etc/fallback/slicer-midas-records.sqlite")
--- etc/conf/config.py
+++ etc/conf/config.py
@@ -1,2 +1,4 @@
+import os
+
 DEBUG = False
-DB_FILE = "../var/slicer-midas-records.sqlite"
+DB_FILE = os.environ.get("SLICER_DOWNLOAD_DB_FILE", "../var/slicer-midas-records.sqlite")
