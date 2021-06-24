8ea0a8f8325b01131bf885f926ab4df7a59d7e5b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-06-24 01:14:19 -0400

    getbuildinfo|download_server|config: Consistently initialize settings
    Consistently initialize database filepath across cron-getbuildinfo and
    and slicer_download_server by implementing these changes:
    
    * Add "slicer_download_server.dbFilePath()" function
    
    * Introduce DB_FALLBACK config variable:
      - optionally initialized with SLICER_DOWNLOAD_DB_FALLBACK env. variable.
      - used to initialize default value in "dbFilePath()" function.
    
    * Remove DB_FILE configuration file entry associated with hard-coded paths.
      - instead default values are set in "dbFilePath()" function
    
    List of possible default values set in "dbFilePath()" function based
    on SLICER_DOWNLOAD_DB_FALLBACK and SLICER_DOWNLOAD_SERVER_API env. variables:
    
    - <app.root_path>/etc/fallback/slicer-girder-records.sqlite
    - <app.root_path>/etc/fallback/slicer-midas-records.sqlite
    - <app.root_path>/var/slicer-girder-records.sqlite
    - <app.root_path>/var/slicer-midas-records.sqlite

--- etc/conf/config-selfserve.py
+++ etc/conf/config-selfserve.py
@@ -1,5 +1,5 @@
 import os
 
+DB_FALLBACK = os.environ.get("SLICER_DOWNLOAD_DB_FALLBACK", True)
 DEBUG = True
-DB_FILE = os.environ.get("SLICER_DOWNLOAD_DB_FILE", "../etc/fallback/slicer-midas-records.sqlite")
 SERVER_NAME = 'localhost:13131'
