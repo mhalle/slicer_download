9eceba1d839e4ceecaaefed78b8d63e3b7f4589f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-06-27 12:17:39 -0400

    bin/cron-parselogs: Refactor to be independent of HOME directory

--- bin/cron-parselogs.sh
+++ bin/cron-parselogs.sh
@@ -1,20 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+
+script_dir=$(cd $(dirname $0) || exit 1; pwd)
+
+ROOT_DIR=$(realpath "${script_dir}/..")
+VIRTUALENV_DIR=$(realpath -m "${ROOT_DIR}/env")
+PYTHON_EXECUTABLE=${VIRTUALENV_DIR}/bin/python
 
 APP=slicer_download
-ROOT=${HOME}/apps/${APP}
-PYTHON="${ROOT}/env/bin/python"
 
-DBFILE="${ROOT}/var/download-stats.sqlite"
-GEOIPFILE="${ROOT}/etc/geoip/db/GeoLite2-City.mmdb"
-STATSDATAFILE="${ROOT}/var/slicer-download-data.json"
+DBFILE="${ROOT_DIR}/var/download-stats.sqlite"
+GEOIPFILE="${ROOT_DIR}/etc/geoip/db/GeoLite2-City.mmdb"
+STATSDATAFILE="${ROOT_DIR}/var/slicer-download-data.json"
 
-APACHELOGS="${HOME}/logs/sites/${APP}/access*"
-# EXTRALOGS="${ROOT}/legacy-logs/*"
+APACHELOGS="${ROOT_DIR}/../logs/sites/${APP}/access*"
+# EXTRALOGS="${ROOT_DIR}/legacy-logs/*"
 EXTRALOGS=""
 
-MODULE="${ROOT}/slicer4-download"
+# Display summary
+echo
+echo "[slicer_getbuildinfo] Using this config"
+echo "  APACHELOGS     : ${APACHELOGS}"
+echo "  DBFILE         : ${DBFILE}"
+echo "  STATSDATAFILE  : ${STATSDATAFILE}"
+echo "  GEOIPFILE      : ${GEOIPFILE}"
+echo
+echo "[slicer_getbuildinfo] Using these directories"
+echo "  ROOT_DIR       : ${ROOT_DIR}"
 
-exec "${PYTHON}" "${ROOT}/etc/slicer_parselogs" \
+echo
+exec "${PYTHON_EXECUTABLE}" "${ROOT_DIR}/etc/slicer_parselogs" \
     --db ${DBFILE} \
     --geoip ${GEOIPFILE} \
     --statsdata ${STATSDATAFILE} \
