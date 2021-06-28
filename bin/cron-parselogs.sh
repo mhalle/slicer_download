4a564f819fad9d79c14c9fd3d2f6477a1fde8e96
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-06-27 23:59:13 -0400

    bin/cron-parselogs: Introduce LOG_DIR and support customization with .start_environment
    Update default access logs location from "../logs/sites/download_slicer_org"
    to "../logs/apps/slicer_download"

--- bin/cron-parselogs.sh
+++ bin/cron-parselogs.sh
@@ -8,6 +8,14 @@ ROOT_DIR=$(realpath "${script_dir}/..")
 VIRTUALENV_DIR=$(realpath -m "${ROOT_DIR}/env")
 PYTHON_EXECUTABLE=${VIRTUALENV_DIR}/bin/python
 
+# Customizing environment
+echo -n "[slicer_getbuildinfo] Looking for ${script_dir}/.start_environment "
+if [ -e "${script_dir}/.start_environment" ]; then
+  source "${script_dir}/.start_environment"
+  echo "[ok]"
+else
+  echo "[not found]"
+fi
 
 SLICER_DOWNLOAD_STATS_DB_FILE="${ROOT_DIR}/var/download-stats.sqlite"
 SLICER_DOWNLOAD_STATS_DATA_FILE="${ROOT_DIR}/var/slicer-download-data.json"
@@ -15,7 +23,10 @@ SLICER_DOWNLOAD_STATS_DATA_FILE="${ROOT_DIR}/var/slicer-download-data.json"
 GEOIP_DB_DIR=${ROOT_DIR}/etc/geoip/db
 GEOIP_DB_FILE="${GEOIP_DB_DIR}/GeoLite2-City.mmdb"
 
-SLICER_DOWNLOAD_ACCESS_LOGS="${ROOT_DIR}/../logs/sites/download_slicer_org/access*"
+LOG_DIR=${LOG_DIR:-$(realpath -m "${ROOT_DIR}/../logs/apps/slicer_download")}
+
+SLICER_DOWNLOAD_ACCESS_LOGS="${LOG_DIR}/access*"
+# SLICER_DOWNLOAD_ACCESS_LOGS="${ROOT_DIR}/../logs/sites/download_slicer_org/access*"
 # EXTRALOGS="${ROOT_DIR}/legacy-logs/*"
 EXTRALOGS=""
 
@@ -29,6 +40,7 @@ echo "  GEOIP_DB_FILE                    : ${GEOIP_DB_FILE}"
 echo
 echo "[slicer_parselogs] Using these directories"
 echo "  ROOT_DIR       : ${ROOT_DIR}"
+echo "  LOG_DIR        : ${LOG_DIR}"
 
 echo
 export PYTHONPATH=${ROOT_DIR}/etc/
